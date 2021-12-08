758175935b846a33c2ce12b1448edb2636322e0f
Ismael <ismaelrajr@gmail.com>
2021-12-08 00:54:34 -0300

    captura elementos da borda da matriz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,17 +6,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rotate-table';
-  lista='';
+  lista: string = '';
   save() {
-    this.lista = '1,2,3,4,5,6,7,8,9'
+    let aa = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]];
     let arrInput = this.lista.split(',');
-    if (Number.isInteger(Math.sqrt(arrInput.length))) {
-      if (Math.sqrt(arrInput.length) % 2) {
-        alert('impar')
-      } else {
-        alert('par')
+    if (arrInput.length > 0) {
+      var a = arrInput.length;
+      if (Number.isInteger(Math.sqrt(arrInput.length))) {
+        if (Math.sqrt(arrInput.length) % 2) {
+          // alert('impar')
+        } else {
+          // alert('par')
+        }
+        this.percorrer(aa, aa, aa.length);
+        console.log(arrInput);
       }
-      console.log(arrInput);
+    }
+  }
+  percorrer (array1: any[][], resultante: any[], tam: any) {
+    if (array1.length > 0) {
+      let aux = [];
+      for (let j=0; j<array1.length-1; j++) {
+        aux.push(array1[0][j]);
+        if (j != array1.length - 1) {
+        } 
+      }
+      for (let i=0; i<array1.length-1; i++) {
+        aux.push(array1[i][array1.length-1]);
+        if (i !=array1.length - 1) {
+        } 
+      }
+      for (let j=array1.length-1; j>0; j--) {
+        aux.push(array1[array1.length-1][j]);
+      }
+      for (let i=array1.length-1; i>0; i--) {
+        aux.push(array1[i][0]);
+      }
+      aux.unshift(aux.pop());
+      console.log(aux);
     }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SaidaComponent } from './saida/saida.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { SaidaComponent } from './saida/saida.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     BrowserAnimationsModule
   ],
   providers: [],
